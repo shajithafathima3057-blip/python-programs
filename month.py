be26361f47a65a078270e2f0a875928b897b8f21
shajithafathima3057-blip <shajithafathima3057@gmail.com>
2025-07-31 14:30:50 +0530

    Create month.py

---
+++ month.py
@@ -0,0 +1,6 @@
+def year(month):
+      print('select',month)
+      
+month=['january','febuary','march','April','may','june','july','august','september','october','november','december']
+for month in month:
+      year(month)
