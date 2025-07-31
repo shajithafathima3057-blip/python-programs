941d2e3a38410a691d1caa8bf4baabd356122043
shajithafathima3057-blip <shajithafathima3057@gmail.com>
2025-07-31 14:38:10 +0530

    Create atm.py

---
+++ atm.py
@@ -0,0 +1,10 @@
+print("ATM")
+print("Insert a Card")
+data1="English"
+print("choose a language:",data1)
+print("Withdraw")
+data2="2530"
+print("Enter a pin number:",data2)
+data3=3000
+print("Enter the amount:", data3)
+print("Take the card")
